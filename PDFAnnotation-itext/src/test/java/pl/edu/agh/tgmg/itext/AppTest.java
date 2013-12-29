0e0c742e0f976336ca8ab98dfcba9c65f3c69a07
Michal Gawarzynski <gawarzynskim@interia.pl>
2013-12-29 12:06:29 +0100

    #IO-27 struktura aplikacji

---
+++ PDFAnnotation-itext/src/test/java/pl/edu/agh/tgmg/itext/AppTest.java
@@ -0,0 +1,20 @@
+package pl.edu.agh.tgmg.itext;
+
+
+import junit.framework.TestSuite;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+@Test(groups = "init")
+public class AppTest 
+{
+
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
